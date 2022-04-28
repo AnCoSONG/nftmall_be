9101e8605acadde9341635f120a05180ebb513b3
AnCoSONG <1197252996@qq.com>
2022-04-28 22:40:01 +0800

    fix: 🐛 add helmet,cookie,compress, set cors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+// import { FastifyReply } from 'fastify';
 
 @Controller()
 export class AppController {
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import fastifyHelmet from '@fastify/helmet';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import fastifyCompress from '@fastify/compress';
+import { fastifyCookie } from '@fastify/cookie';
 
 async function bootstrap() {
   const fastifyAdapter = new FastifyAdapter({ logger: true });
+  fastifyAdapter.register(fastifyHelmet);
+  fastifyAdapter.register(fastifyCompress);
+  fastifyAdapter.register(fastifyCookie, {
+    secret: 'anco',
+  });
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     fastifyAdapter,
+    {
+      cors: {
+        origin:
+          process.env.NODE_ENV === 'dev'
+            ? 'http://localhost:3000'
+            : 'https://jinyuanshuzi.com',
+        credentials: true,
+      },
+    },
   );
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
