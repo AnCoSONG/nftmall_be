74a2471299c2691dec1be1c8a2de94f9eb8376f2
AnCoSONG <1197252996@qq.com>
2022-04-28 20:51:31 +0800

    feat: 🎸 add fastify support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const fastifyAdapter = new FastifyAdapter({ logger: true });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    fastifyAdapter,
+  );
   await app.listen(3000);
 }
 bootstrap();
