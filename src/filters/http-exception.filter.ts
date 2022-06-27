19d6a102cd77afc47c1503bc9633a97ab29dca28
AnCoSONG <1197252996@qq.com>
2022-06-28 02:46:49 +0800

    fix: 🐛 auto clear cookie

--- src/filters/http-exception.filter.ts
+++ src/filters/http-exception.filter.ts
@@ -3,14 +3,18 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  Logger,
 } from '@nestjs/common';
+import { FastifyReply, FastifyRequest } from 'fastify';
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name)
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    const response = ctx.getResponse();
-    const request = ctx.getRequest();
+    const response = ctx.getResponse<FastifyReply>();
+    const request = ctx.getRequest<FastifyRequest>();
     const statusCode = exception.getStatus();
+    const message = exception.message;
 
     const errorResponse = {
       statusCode,
@@ -19,6 +23,14 @@ export class HttpExceptionFilter implements ExceptionFilter {
       error: exception.name,
       url: request.url,
     };
+    if (statusCode === 401) {
+      if (!message.startsWith('admin')) {
+        this.logger.debug('auth error, clear cookie')
+        // 如果401 且不是以admin开头的message => 直接clearcookie
+        response.clearCookie('tt', { path: '/' });
+        response.clearCookie('xc', { path: '/' });
+      }
+    }
 
     response.status(exception.getStatus()).send(errorResponse);
   }
