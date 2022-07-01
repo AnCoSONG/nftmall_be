b01226a57b50534a0a1981f64fa1408b1e1048fd
AnCoSONG <1197252996@qq.com>
2022-07-02 03:18:29 +0800

    feat: 🎸 add support for admin guard

--- src/auth/guards/admin.guard.ts
+++ src/auth/guards/admin.guard.ts
@@ -10,8 +10,6 @@ import { AuthService } from '../auth.service';
 import { ConfigService } from '@nestjs/config';
 import * as AES from 'crypto-js/aes';
 import * as Utf8 from 'crypto-js/enc-utf8';
-import * as Latin1 from 'crypto-js/enc-latin1'
-import * as Hex from 'crypto-js/enc-hex';
 import * as ECB from 'crypto-js/mode-ecb';
 import * as PKCS7 from 'crypto-js/pad-pkcs7';
 import { randomBytes } from 'crypto';
@@ -21,10 +19,10 @@ export class AdminGuard implements CanActivate {
   constructor(private readonly configService: ConfigService) {}
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<FastifyRequest>();
-    if (!request.cookies['Admin-Token']) {
+    if (!request.headers['y-token']) {
       throw new UnauthorizedException('no auth token provided')
     }
-    const adminToken = request.cookies['Admin-Token'];
+    const adminToken = request.headers['y-token'] as string;
     // * encrypt demo
     // console.log(`${Date.now()}-admin-${randomBytes(8).toString('hex')}-${this.configService.get('admin.secret')}`)
     // const encrypted = AES.encrypt(`${Date.now()}-admin-${randomBytes(8).toString('hex')}-${this.configService.get('admin.secret')}`, Utf8.parse(this.configService.get('admin.secret')), {
@@ -34,15 +32,18 @@ export class AdminGuard implements CanActivate {
     // console.log(encrypted)
     // admintoken = aes(timestamp-admin-nonce-secret)
     const decryptedToken = AES.decrypt(
-      adminToken, 
-      Utf8.parse(this.configService.get('admin.secret')),
+      adminToken,
+      // Utf8.parse(this.configService.get('admin.secret')),
+      Utf8.parse(this.configService.get('admin.secret')), // key length 128/8=16 256/8=32
       {
         mode: ECB,
         padding: PKCS7,
       },
     ).toString(Utf8);
+    // console.log('dec', decryptedToken)
     // console.log(decryptedToken)
     const tempArr = decryptedToken.split('-');
+    // console.log(tempArr)
     // 验证格式
     if (tempArr.length !== 4) {
       throw new UnauthorizedException('invalid token');
